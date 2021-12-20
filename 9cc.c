f995022bc16459b0f2e23566a026d6087a852ae8
osorosia <64416398+osorosia@users.noreply.github.com>
2021-12-20 16:24:25 +0900

    加減算を対応

--- 9cc.c
+++ 9cc.c
@@ -8,10 +8,34 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Invalid argument!\n");
 		return (1);
 	}
+
+	char *p = argv[1];
+
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
-	printf("\tmov rax, %d\n", atoi(argv[1]));
+	printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+	while (*p)
+	{
+		if (*p == '+') 
+		{
+			p++;
+			printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if (*p == '-') 
+		{
+			p++;
+			printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "unexpected character: '%c'\n", *p);
+		return (1);
+	}
+
 	printf("\tret\n");
 	return (0);
 }
